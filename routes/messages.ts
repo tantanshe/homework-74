d732ccf8905d452c201dfbd80884f3c0fc280a26
Tatiana Shishenina <tatyana020711@gmail.com>
2024-08-04 17:27:55 +0600

    Realised logic for creating files with POST requests and for showing the last 5 messages

--- routes/messages.ts
+++ routes/messages.ts
@@ -1,13 +1,34 @@
 import express from "express";
+import {promises as fs} from 'fs';
 
 const messagesRouter = express.Router();
+const path = './messages';
 
-messagesRouter.get("/", (req, res) => {
-    res.send("Messages are here");
+messagesRouter.post("/", async (req, res) => {
+    const messageData = req.body;
+    const datetime = new Date().toISOString();
+    const fileName = `./messages/${datetime}.txt`;
+    const message = {
+        ...messageData,
+        datetime,
+    };
+    await fs.writeFile(fileName, JSON.stringify(message));
+    console.log(req.body);
+    res.send(req.body);
 })
 
-messagesRouter.post("/", (req, res) => {
-    res.send(req.body);
+messagesRouter.get("/", async (req, res) => {
+    const files = await fs.readdir(path);
+    const sortedFiles = files.sort().reverse();
+    const recentFiles = sortedFiles.slice(0, 5);
+    const messages = await Promise.all(
+        recentFiles.map(async (file) => {
+            const filePath = path + '/' + file;
+            const fileContent = await fs.readFile(filePath);
+            return JSON.parse(fileContent.toString());
+        })
+    );
+    res.send(messages);
 })
 
 export default messagesRouter;
